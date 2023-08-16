a1752d2cdb9e94b8dfa264c9385ea1ceeee40d45
Crowdbotics <team@crowdbotics.com>
2023-08-16 12:54:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-flower-42872",
+  url: "https://noisy-flower-42872.botics.co",
+  api: "https://noisy-flower-42872.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
